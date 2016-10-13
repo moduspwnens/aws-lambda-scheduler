b39ff04adde64d340b2db3858e7b867d98efd21d
Benn Linger <benn@linger.com>
2016-10-13 17:29:29 -0400

    Progress on deploy script.

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# aws-lambda-scheduler
+# Serverless AWS Lambda Scheduling API
 
 [![Build Status](https://travis-ci.org/moduspwnens/aws-lambda-scheduler.svg?branch=master)](https://travis-ci.org/moduspwnens/aws-lambda-scheduler)
+
+An easy-to-deploy, scalable API for scheduling the invocation of Lambda functions with no fixed costs.
+
+Not yet functional--still a work in progress.
+
+## Why?
+
+AWS doesn't provide a way out-of-the-box for scheduling the execution of Lambda functions. However, there are plenty of use cases:
+ * Moving / deleting DynamoDB data after it reaches a certain age
+ * Terminating an instance after it's been stopped for a certain amount of time
+ * Sending an e-mail a certain amount of time after an event occurs
+
+You can work around these sometimes through a scheduled CloudWatch event, but even that has limits. You're limited on the amount of CloudWatch event rules you can have, you have to architect around the invocations being effectively stateless (you'd need to look up what e-mails to send, for example), and you're limited to one invocation per minute. 
+
+This allows you to schedule the invocation of a Lambda function with a specific payload at a specific time. Or a lot of them, with lots of payloads at varying times.
+
+## How to deploy
+
+Deploy from your local machine. Just requires git and Python 2.7.
+
+```
+$ git clone https://github.com/moduspwnens/aws-lambda-scheduler.git
+$ cd aws-lambda-scheduler
+$ python deploy.py
+```
+
+Tested on Mac, Linux, and Windows.
+
+All resources are contained as part of a CloudFormation stack, which you can view on the [CloudFormation web page](https://console.aws.amazon.com/cloudformation/home). To uninstall, simply delete the stack from here.
+
--- deploy.py
+++ deploy.py
@@ -2,10 +2,17 @@
 
 from __future__ import print_function
 
-import os, sys, time, shutil, zipfile, subprocess, argparse, json
+import os, sys, time, shutil, zipfile, subprocess, argparse, json, tempfile
+import hashlib, logging
+
+try:
+    from urllib2 import urlopen
+except:
+    from urllib.request import urlopen
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--stack-name", default="lambda-scheduler-default", help="Name for the CloudFormation stack.")
+parser.add_argument("--clean", action="store_true", help="Remove all build artifacts first.")
 parser.add_argument("--build-lambda-functions-only", action="store_true", help="Just build the Lambda functions and exit.")
 
 
@@ -14,19 +21,101 @@ repo_dir = os.path.dirname(os.path.realpath(__file__))
 cf_template_path = os.path.join(repo_dir, "lambda-scheduler.yaml")
 s3_template_upload_key = "cf-stack-template.yaml"
 build_dir = os.path.join(repo_dir, "build")
+deploy_venv_dir = os.path.join(build_dir, "deploy-venv")
+deploy_pip_dir = os.path.join(build_dir, "deploy-pip")
 functions_source_dir = os.path.join(repo_dir, "lambda/functions")
 
+sys.path.insert(1, deploy_pip_dir)
 
-def verify_cloudformation_deploy_dependencies():
-    try:
-        import boto3
-    except:
-        raise Exception("Unable to load boto3. Try \"pip install boto3\".")
+def create_deploy_virtualenv():
     
+    import pip
+    
+    virtualenv_install_verified = False
     try:
-        import yaml
+        exit_code = pip.main(["show", "virtualenv"])
+        virtualenv_install_verified = (exit_code == 0)
     except:
-        raise Exception("Unable to load yaml. Try \"pip install PyYAML\".")
+        pass
+    
+    if not virtualenv_install_verified:
+        print("Installing virtualenv.")
+        
+        exit_code = pip.main(["install", "-t", deploy_pip_dir, "virtualenv"])
+        
+        if exit_code != 0:
+            raise Exception("Non-zero exit code ({}) received from attempted virtualenv install.".format(exit_code))
+    
+    print("Creating virtualenv.")
+    p = subprocess.Popen(
+        [sys.executable, os.path.join(deploy_pip_dir, "virtualenv.py"), deploy_venv_dir]
+    )
+    
+    exit_code = p.wait()
+    if exit_code != 0:
+        raise Exception("Non-zero exit code ({}) received from attempted virtualenv creation.".format(exit_code))
+
+def install_local_pip():
+        
+        print("Installing pip.")
+        
+        pip_installer_script_content = urlopen("https://bootstrap.pypa.io/get-pip.py").read()
+        
+        temp_file_path = None
+        with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
+            temp_file_path = tmpfile.name
+        
+        open(temp_file_path, "w").write(pip_installer_script_content)
+        
+        os.makedirs(deploy_pip_dir)
+        
+        p = subprocess.Popen(
+            [sys.executable, temp_file_path, "-t", deploy_pip_dir]
+        )
+        exit_code = p.wait()
+        
+        os.unlink(temp_file_path)
+        
+        if exit_code != 0:
+            raise Exception("Non-zero exit code ({}) received from attempted pip install.".format(exit_code))
+
+def verify_deploy_env():
+    
+    if not os.path.isdir(deploy_pip_dir):
+        install_local_pip()
+    
+    if not os.path.isdir(deploy_venv_dir):
+        create_deploy_virtualenv()
+    
+    # Windows
+    venv_activate_script_path = os.path.join(deploy_venv_dir, "Scripts", "activate_this.py")
+    
+    # Everything else
+    if not os.path.exists(venv_activate_script_path):
+        venv_activate_script_path = os.path.join(deploy_venv_dir, "bin", "activate_this.py")
+    
+    execfile(venv_activate_script_path, dict(__file__=venv_activate_script_path))
+    
+    # Windows
+    venv_pip_path = os.path.join(deploy_venv_dir, "Scripts", "pip")
+    
+    # Everything else
+    if not os.path.exists(venv_pip_path):
+        venv_pip_path = os.path.join(deploy_venv_dir, "bin", "pip")
+    
+    p = subprocess.Popen(
+        [
+            venv_pip_path,
+            "install",
+            "-r",
+            os.path.join(repo_dir, "deploy-requirements.txt")
+        ]
+    )
+    
+    exit_code = p.wait()
+    
+    if exit_code != 0:
+        raise Exception("Non-zero exit code ({}) received from attempted deployment dependencies install.".format(exit_code))
 
 def verify_aws_credentials_set():
     caller_identity_arn = boto3.client("sts").get_caller_identity()["Arn"]
@@ -40,7 +129,9 @@ def ensure_build_dir_exists():
             raise Exception("Error creating build directory at {}.".format(build_dir))
 
 def build_lambda_function_environments():
-
+    
+    import checksumdir
+    
     function_source_dir_list = []
 
     for dir_name, subdir_list, file_list in os.walk(functions_source_dir):
@@ -53,9 +144,29 @@ def build_lambda_function_environments():
     for each_function_source_dir in function_source_dir_list:
     
         each_function_name = each_function_source_dir.split("/")[-1]
-    
+        
+        each_function_build_metadata_file_path = os.path.join(build_dir, "{}.json".format(each_function_name))
+        
+        each_function_previous_build_metadata = {
+            "source": "",
+            "zip": ""
+        }
+        
+        if os.path.exists(each_function_build_metadata_file_path):
+            each_function_previous_build_metadata = json.loads(open(each_function_build_metadata_file_path).read())
+        
+        source_dir_hash = checksumdir.dirhash(each_function_source_dir)
+        
+        zip_output_path = os.path.join(build_dir, "{}.zip".format(each_function_name))
+        
+        if os.path.exists(zip_output_path):
+            if each_function_previous_build_metadata["source"] == source_dir_hash:
+                if file_md5_checksum(zip_output_path) == each_function_previous_build_metadata["zip"]:
+                    print("{} already built.".format(each_function_name))
+                    continue
+        
         print("Building Lambda function: {}".format(each_function_name))
-    
+        
         function_build_dir = os.path.join(build_dir, each_function_name)
     
         if os.path.exists(function_build_dir):
@@ -66,6 +177,7 @@ def build_lambda_function_environments():
         pip_requirements_path = os.path.join(function_build_dir, "requirements.txt")
     
         if os.path.exists(pip_requirements_path):
+            
             print("Installing dependencies.")
             p = subprocess.Popen(
                 ["pip", "install", "-r", pip_requirements_path, "-t", function_build_dir],
@@ -86,6 +198,13 @@ def build_lambda_function_environments():
             os.unlink("{}.zip".format(build_zip_path))
     
         shutil.make_archive(build_zip_path, "zip", function_build_dir)
+        
+        new_build_metadata = {
+            "source": source_dir_hash,
+            "zip": file_md5_checksum(zip_output_path)
+        }
+        
+        open(each_function_build_metadata_file_path, "w").write(json.dumps(new_build_metadata, indent=4))
     
         print("Successfully built Lambda function: {}.".format(each_function_name))
     
@@ -138,7 +257,7 @@ def create_base_cloudformation_stack(cf_stack_name):
     response = cloudformation_client.create_stack(
         StackName = cf_stack_name,
         TemplateBody = json.dumps(cf_template_object, indent=4),
-        Capabilities = ["CAPABILITY_NAMED_IAM"]
+        Capabilities = ["CAPABILITY_IAM"]
     )
 
     stack_id = response["StackId"]
@@ -214,7 +333,7 @@ def update_base_stack_to_full_stack(stack_id, s3_bucket_name):
             s3_template_upload_key
         ),
         UsePreviousTemplate = False,
-        Capabilities = ["CAPABILITY_NAMED_IAM"]
+        Capabilities = ["CAPABILITY_IAM"]
     )
 
     last_status = "UPDATE_IN_PROGRESS"
@@ -238,14 +357,23 @@ def update_base_stack_to_full_stack(stack_id, s3_bucket_name):
     if last_status != "UPDATE_COMPLETE":
         raise Exception("Stack reached unexpected status: {}".format(last_status))
 
+def file_md5_checksum(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
 if __name__ == "__main__":
     
     args = parser.parse_args()
     
-    if not args.build_lambda_functions_only:
-        verify_cloudformation_deploy_dependencies()
+    if args.clean and os.path.exists(build_dir):
+        shutil.rmtree(build_dir)
     
     ensure_build_dir_exists()
+    
+    verify_deploy_env()
     
     function_source_dir_list = build_lambda_function_environments()
     
@@ -253,6 +381,9 @@ if __name__ == "__main__":
         sys.exit(0)
         
     import boto3, yaml
+    
+    logging.getLogger('boto3').setLevel(logging.WARNING)
+    logging.getLogger('botocore').setLevel(logging.WARNING)
     
     verify_aws_credentials_set()
     
